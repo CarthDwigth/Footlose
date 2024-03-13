2a51a60a31694652afa4e431f180fc5cfde63c23
CarthDwigth <147659623+CarthDwigth@users.noreply.github.com>
2024-03-13 19:21:21 -0400

    mm.2
    mmm.2

--- Mundo/Creditos.gd
+++ Mundo/Creditos.gd
@@ -1,9 +1,13 @@
 extends Node2D
 
 @onready var Botellas = $CanvasLayer/Label
+@onready var Dinero = $CanvasLayer/Label2
+@onready var Pescado = $CanvasLayer/Label3
 
 func _ready():
 	Botellas.text = "Collected recyclables: "+ str(Status0.BotellasTo)
+	Dinero.text = "Total Money: "+ str(Status0.Moneyto)
+	Pescado.text = "Total Fish: "+ str(Status0.pescadosTo)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
